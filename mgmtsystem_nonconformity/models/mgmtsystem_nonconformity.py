e94bab19e11c8d0b39a0b5496ae6af6619bbfdfd
Edgar Rivero <edgar@vauxoo.com>
2016-04-13 17:01:48 -0500

    [ADD] Due to an ISO/TS 16949 requirement when occurs a nonconformity for any failure that could result in a probable shipment of nonconforming product, the document nonconformity requires it can be associate a product, in response to this requirement the field product_id is added to the model mgmtsystem.nonconformity

--- mgmtsystem_nonconformity/models/mgmtsystem_nonconformity.py
+++ mgmtsystem_nonconformity/models/mgmtsystem_nonconformity.py
@@ -203,6 +203,8 @@ class MgmtsystemNonconformity(models.Model):
         'Preventive action',
         domain="[('nonconformity_id', '=', id)]",
     )
+    
+    product_id = fields.Many2one('product.product', 'Product')
 
     @property
     @api.multi
